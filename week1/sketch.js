82a08898dfa13eaa24e5755fef5eb24591f6a739
alsayhe1234 <yh6718@nyu.edu>
2026-09-09 01:48:09 -0400

    changing the definition and source

--- week1/sketch.js
+++ week1/sketch.js
@@ -2,14 +2,14 @@
 // this is the only part of the file you need to edit!
 
 const quotes = [
-  { text: "the intersection of programming, design, and art", source: "Scott" },
-  { text: "a chance for code to be wrong", source: "Sara" },
-  { text: "surprise, exploration, and aesthetic exploration", source: "Sally" },
-  { text: "process over results", source: "Sam" },
-  { text: "form over function", source: "Sean" },
-  { text: "iterative and experimental", source: "Scooby" },
-  { text: "a community of practice", source: "Seth" },
-  { text: "an opportunity to stick it to the man", source: "Shifty-eyed Susan" }
+  { text: "programming for artistic expression, not utility.", source: "Quiet Canvas Staff, 2026" },
+  { text: "the practice of combining programming techniques with artistic expression in order to create aesthetically pleasing and optimized interactive digital experiences.", source: "SMU Meadows School of the Arts, 2024" },
+  { text: "not a tool; it is a new material for expression.", source: "John Maeda, 1999" },
+  { text: "a mode of computer-based work “that emphasizes the expressivity of computer programming beyond something pragmatic and functional", source: "John Maeda, 2004" },
+  { text: "a new interdisciplinary art form that bridges the gap between technologists and artists.", source: "Ahmad Moussa, 2023" },
+  { text: "a loosely defined term used to describe a wide range of artistic practices that use computer code as a medium.", source: "Raphaël de Courville, naoto-hieda, and Alexandre B A Villares, 2021" },
+  { text: "where programming becomes a medium for self-expression and innovation.", source: "Laylah Bulman, 2025" },
+  { text: "a process, based on exploration, iteration, reflection and discovery, where code is used as the primary medium to create a wide range of media artifacts.", source: "Mark Mitchell, Oliver C. Bown, 2013" }
 ];
 // no need to edit anything below this line! 
 // if you have made an error, you can check your history to see what might have gone wrong
@@ -21,8 +21,8 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   // Seed with millis() so each page load gives a different quote.
   randomSeed(millis());
-  textSize(32);
-  fill(10, 255, 10);
+  textSize(36);
+  fill(10, 255, 236);
   // calls the function to pick a quote
   pickQuote();
 }
@@ -33,18 +33,19 @@ function pickQuote() {
 }
 
 function draw() {
-  background(255, 20, 250); // set the background color
+  background(128, 56, 236); // set the background color
   drawQuote();  // draw the quote on screen
 }
 
 function drawQuote() {   // draw text
+  fill(10,31,236);
+  textFont("Helvetica");
   textAlign(CENTER, CENTER);
   text("Creative Coding is.....", width / 2, height / 2 - 48);
   textStyle(BOLD);
   text("'" + current.text + "'", width / 2, height / 2);
   textAlign(RIGHT, CENTER);
   text("-" + current.source, width - 100, height - 100);
-
 }
 
 function windowResized() {
